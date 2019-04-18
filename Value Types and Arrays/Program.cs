3bca17c91d38780bc89d3ac0ad793b3eb3872ad6
Ryan White <WHITER23@my.erau.edu>
2019-04-18 10:25:43 -0400

    test

---
+++ Value Types and Arrays/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Value_Types_and_Arrays
+{
+    internal class Program
+    {
+        private static void Main(string[] args) => Console.WriteLine("Hello World!");
+    }
+}
